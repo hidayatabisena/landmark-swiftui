3b352d443f9650c4710f5dd95050fea8b2e8c2f2
Han Sena <abisena@outlook.com>
2023-12-13 19:15:04 +0700

    Completing the Data 2️⃣

--- Landmark/Model/ModelData.swift
+++ Landmark/Model/ModelData.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
+var landmarks: [Landmark] = load("landmarkData.json")
+
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
