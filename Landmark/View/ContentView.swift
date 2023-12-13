ee1f95a283a03c6281f92c14c5129afab09deba3
Han Sena <abisena@outlook.com>
2023-12-13 19:48:35 +0700

    Add Landmark Detail 2️⃣

--- Landmark/View/ContentView.swift
+++ Landmark/View/ContentView.swift
@@ -9,38 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            
-            CircleImageView()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetailView()
+                } label: {
+                    LandmarkRowView(landmark: landmark)
                 }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                
-                Text("Descriptive text goes here")
             }
-            .padding()
-            
-            Spacer()
-            
-        } //: VSTACK
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
     }
 }
 
